768c57ef28b387980edd02b596792c52ea37da3c
vivek006 <vivekranjan006@gmail.com>
2018-04-05 15:05:07 +0530

    changes for debug

--- application/views/front/layout/common/slider.php
+++ application/views/front/layout/common/slider.php
@@ -3,8 +3,8 @@
     <div class="row">
       <div class="col m8 l8 s12">
         <div class="head-line">
-          <h3  class="white-text lighten-2">Looking for upgrading the Skills,<br>We are here to help you</h3>
-          <h5 class="white-text lighten-6">S2Know brings IT Courses for students and professionals. We believe in Quality educaction with new technologies , Focus  will be more on basics with workshops.</h5>
+         <!--  <h3  class="white-text lighten-2">Looking for upgrading the Skills,<br>We are here to help you</h3>
+          <h5 class="white-text lighten-6">S2Know brings IT Courses for students and professionals. We believe in Quality educaction with new technologies , Focus  will be more on basics with workshops.</h5> -->
         </div>
       </div>
       <img src="<?php echo base_url()."assets/front/"; ?>img/Capturenrew.png" alt="">
