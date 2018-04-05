5d88529959078c609a0396883eece9345eb379e4
vivek006 <vivekranjan006@gmail.com>
2018-04-06 00:34:31 +0530

    changes for debug

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+echo 'sdfsdfdsfsd';exit;
+
 /*
 | -------------------------------------------------------------------------
 | URI ROUTING
