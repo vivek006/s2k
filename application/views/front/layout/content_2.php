d4853355a7e6e73f68369970ebc09208c50855ff
vivek006 <vivekranjan006@gmail.com>
2018-04-05 22:57:05 +0530

    changes for debug

---
+++ application/views/front/layout/content_2.php
@@ -0,0 +1,15 @@
+
+<section id="who-we-are" class="who-we-are bg-grey">
+	<div class="container">
+		<div class="row">
+		  <div class="col s12">
+			<div class="title">
+			  <h4>WHO WE ARE</h4>
+			  <p class="tex-justify">S2Know is a company that has introduced a new concept in the technical training to help fresher’s understand the different opportunities they can get after completing their engineering. While in the college students often dream about getting placements but end up in confusion as they are unaware of the domain they should choose. Here comes the S2Know which will guide the aspirants during the college time only so that while completing their degree they are well versed with the insights of the IT market.</p>
+			 
+			</div>
+			
+		  </div>
+		</div>
+	</div>
+</section>
